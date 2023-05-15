b5dccb651951adc36f8fb2d918b1092fc36565fe
Alexander Alves <vendas77.rj@gmail.com>
2023-05-15 18:57:16 -0300

    Exercicio 14

---
+++ exercicios_php_completo/05_op_e_expressoes/11_exercicio_14/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  $saudacao = "Sr.";
+  $nome = "Kelvin";
+  $sobrenome = "Gustavo";
+
+  $frase = "O " . $saudacao . " " . $nome . " " . $sobrenome . ", vem hoje para a reunião as 14h";
+
+  echo $frase;
