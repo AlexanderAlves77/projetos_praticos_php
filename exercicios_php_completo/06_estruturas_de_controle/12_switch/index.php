f483fa840237830aaacf38dccc5b2c5f45d713ea
Alexander Alves <vendas77.rj@gmail.com>
2023-05-24 08:09:57 -0300

    Estrutura de controle switch

---
+++ exercicios_php_completo/06_estruturas_de_controle/12_switch/index.php
@@ -0,0 +1,31 @@
+<?php
+
+  $x = 5;
+
+  switch($x) {
+
+    case 0:
+      echo "X é igual a 0 <br>";
+      break;
+    case 1:
+      echo "X é igual 1 <br>";
+      break;
+    default:
+      echo "X não é igual a nenhum dos valores mencionados <br>";
+
+  }
+
+  $y = "";
+
+  switch($y) {
+
+    case "Alexander":
+      echo "O nome é Alexander";
+      break;
+    case "Kelvin":
+      echo "O nome é Kelvin";
+      break;
+    default:
+      echo "O nome não foi encontrado";
+      
+  }
