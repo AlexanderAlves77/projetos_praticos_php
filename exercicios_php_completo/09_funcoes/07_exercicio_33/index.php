2bf945197b08da6cb0e3d68f480960ea4931197e
Alexander Alves <vendas77.rj@gmail.com>
2023-05-29 17:55:55 -0300

    Exercicio 33

---
+++ exercicios_php_completo/09_funcoes/07_exercicio_33/index.php
@@ -0,0 +1,10 @@
+<?php
+
+  function apresentarPessoa($nome, $idade, $genero) {
+
+    echo "Olá eu sou $genero $nome e tenho $idade anos <br>";
+
+  }
+
+  apresentarPessoa("Kelvin", 22, "o");
+  apresentarPessoa("Sara", 9, "a");
