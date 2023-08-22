4eddab005f08fdfb28cf76306314d821ac564aa1
Alexander Alves <vendas77.rj@gmail.com>
2023-08-22 17:28:02 -0300

    Woo commerce file

---
+++ newspaper_customizer/woocommerce.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<div class="container pagecontent">
+    <div id="sitemain" class="sitefull">
+        <div class="contentarea">      
+		    <?php woocommerce_content(); ?>     
+        </div>
+    </div>
+</div>	
+<?php get_footer(); ?>
